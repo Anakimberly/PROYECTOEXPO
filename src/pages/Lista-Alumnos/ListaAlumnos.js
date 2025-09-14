f3c9cc2d7af4c02cea832f01a1184dbc6d0508fe
Anakimberly <22620053@tlaxiaco.tecnm.mx>
2025-09-14 01:40:18 -0600

    se creo el appbar

--- src/pages/Lista-Alumnos/ListaAlumnos.js
+++ src/pages/Lista-Alumnos/ListaAlumnos.js
@@ -1,18 +1,41 @@
-import{ View, Text, StatusBar, Platform,StyleSheet } from 'react-native';
+import{ View, Text, StatusBar, Platform,StyleSheet,SafeAreaView } from 'react-native';
+import { FontAwesome, MaterialIcons, Ionicons } from '@expo/vector-icons';
 
 export const ListaAlumnos = () => {
     return (
-        <View style={styles.mainS}>
-            <Text>Hola</Text>
-        </View>
+        <SafeAreaView style={style.mainS}>
+            {/* inicio de AppBar */}
+            <View style={style.appBar}>
+                <Ionicons name="arrow-back" size={30} color={'#000000ff'} />
+                <Text style={style.appBarTitle}>Lista de Alumnos</Text>
+                </View>
+
+            {/* fin de AppBar */}
+        </SafeAreaView>
     )
 }
 
-const styles = StyleSheet.create({ 
+const style = StyleSheet.create({ 
  mainS:{
         flex:1,
         backgroundColor: '#b4aee9ff',
         paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight:44,
-        padding: 50
+        // padding: 50
+    },
+    appBar:{
+        height:50,
+        width:'100%',
+        backgroundColor:'#ffffffff',
+        flexDirection:'row',
+        alignItems:'center',
+         justifyContent:'space-between',
+        paddingHorizontal:16,
+        
+       
+
+    },
+    appBarTitle:{
+        fontWeight:'bold',
+        fontSize:16,
     },
 });
