8c0c82d4bb7354ac321999c8dd8c638cd1277bbc
Anakimberly <22620053@tlaxiaco.tecnm.mx>
2025-09-13 22:50:17 -0600

    cree un input para la descripcion en el apartado del formulario

--- src/pages/Formulario.js
+++ src/pages/Formulario.js
@@ -2,9 +2,11 @@ import { Platform, StatusBar, StyleSheet,SafeAreaView, Text,TextInput } from "re
 
 export const Formulario = () => {
   return (
-   <SafeAreaView style={ style.mainS}>
-   <Text>Nombre:</Text>
-<TextInput placeholder="Escribe tu nombre"/> 
+   <SafeAreaView style={ style.mainS}> 
+   <Text style ={style.label}>Nombre:</Text>
+<TextInput style ={style.input} placeholder="Escribe tu nombre"/> 
+ <Text style ={style.label}>Descripcion:</Text>
+<TextInput multiline={true} numberOfLines={4} style ={[style.input,style.textArea]} placeholder="Describe tu personalidad"/> 
         
     </SafeAreaView>
   )
@@ -18,4 +20,19 @@ export const Formulario = () => {
           paddingTop: Platform.OS === 'android' ? StatusBar.currentHeigth:44,
           padding: 16
         },
+        label:{
+fontWeight: 'bold',
+marginTop:10,
+marginBottom:10
+        },
+        input:{
+          borderWidth:1,
+          borderColor:'#aaa',
+          borderRadius:10,
+
+        },
+      textArea:{
+        height:100,
+        textAlignVertical:'top'
+      }
   })
